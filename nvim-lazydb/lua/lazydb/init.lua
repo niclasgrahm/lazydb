24848b2bf0fdb6ddc08b32af0395af3922c9c04a
Niclas Grahm <niclas.grahm@propell.dev>
2026-04-19 21:37:00 +0200

    show/hide active session in neovim

--- nvim-lazydb/lua/lazydb/init.lua
+++ nvim-lazydb/lua/lazydb/init.lua
@@ -5,13 +5,18 @@ local M = {}
 local state = {
   buf = nil,
   win = nil,
+  augroup = nil,
 }
 
-local function is_open()
+local function buf_alive()
+  return state.buf ~= nil and vim.api.nvim_buf_is_valid(state.buf)
+end
+
+local function is_visible()
   return state.win ~= nil and vim.api.nvim_win_is_valid(state.win)
 end
 
-local function create_float()
+local function create_float(buf)
   local opts = config.options
   local scale = opts.floating_window_scaling_factor
   local columns = vim.o.columns
@@ -21,8 +26,6 @@ local function create_float()
   local height = math.floor(lines * scale)
   local col = math.floor((columns - width) / 2)
   local row = math.floor((lines - height) / 2)
-
-  local buf = vim.api.nvim_create_buf(false, true)
 
   local win = vim.api.nvim_open_win(buf, true, {
     relative = "editor",
@@ -36,50 +39,109 @@ local function create_float()
 
   vim.api.nvim_set_option_value("winblend", opts.floating_window_winblend, { win = win })
 
-  return buf, win
+  return win
 end
 
-function M.open()
-  if is_open() then
-    vim.api.nvim_set_current_win(state.win)
-    return
-  end
+local function setup_autocommands(buf)
+  local ag = vim.api.nvim_create_augroup("LazyDB", { clear = true })
+  state.augroup = ag
 
-  local buf, win = create_float()
-  state.buf = buf
-  state.win = win
-
-  vim.fn.termopen(config.options.lazydb_command, {
-    on_exit = function()
-      if state.win and vim.api.nvim_win_is_valid(state.win) then
-        vim.api.nvim_win_close(state.win, true)
+  vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter" }, {
+    group = ag,
+    buffer = buf,
+    callback = function()
+      if buf_alive() then
+        vim.cmd("startinsert")
       end
-      if state.buf and vim.api.nvim_buf_is_valid(state.buf) then
-        vim.api.nvim_buf_delete(state.buf, { force = true })
-      end
-      state.buf = nil
-      state.win = nil
     end,
   })
 
+  vim.api.nvim_create_autocmd({ "FocusGained", "TermLeave" }, {
+    group = ag,
+    callback = function()
+      if is_visible() and vim.api.nvim_get_current_buf() == state.buf then
+        vim.defer_fn(function()
+          if is_visible() and vim.api.nvim_get_current_buf() == state.buf then
+            vim.cmd("startinsert")
+          end
+        end, 0)
+      end
+    end,
+  })
+end
+
+local function spawn()
+  local buf = vim.api.nvim_create_buf(false, true)
+  state.buf = buf
+
+  state.win = create_float(buf)
+
+  vim.fn.termopen(config.options.lazydb_command, {
+    on_exit = function()
+      vim.schedule(function()
+        if state.augroup then
+          vim.api.nvim_del_augroup_by_id(state.augroup)
+          state.augroup = nil
+        end
+        if state.win and vim.api.nvim_win_is_valid(state.win) then
+          vim.api.nvim_win_close(state.win, true)
+        end
+        if state.buf and vim.api.nvim_buf_is_valid(state.buf) then
+          vim.api.nvim_buf_delete(state.buf, { force = true })
+        end
+        state.buf = nil
+        state.win = nil
+      end)
+    end,
+  })
+
+  setup_autocommands(buf)
+  vim.api.nvim_buf_set_keymap(buf, "t", "<Esc><Esc>", "<C-\\><C-n>:LazyDB<CR>", { noremap = true, silent = true })
   vim.cmd("startinsert")
 end
 
-function M.close()
-  if not is_open() then
+function M.open()
+  if is_visible() then
+    vim.api.nvim_set_current_win(state.win)
+    vim.cmd("startinsert")
+    return
+  end
+
+  if buf_alive() then
+    state.win = create_float(state.buf)
+    vim.cmd("startinsert")
+    return
+  end
+
+  spawn()
+end
+
+function M.hide()
+  if not is_visible() then
     return
   end
   vim.api.nvim_win_close(state.win, true)
-  if state.buf and vim.api.nvim_buf_is_valid(state.buf) then
+  state.win = nil
+end
+
+function M.kill()
+  if buf_alive() then
     vim.api.nvim_buf_delete(state.buf, { force = true })
+  end
+  if state.win and vim.api.nvim_win_is_valid(state.win) then
+    vim.api.nvim_win_close(state.win, true)
+  end
+  if state.augroup then
+    vim.api.nvim_del_augroup_by_id(state.augroup)
+    state.augroup = nil
   end
   state.buf = nil
   state.win = nil
 end
 
 function M.toggle()
-  if is_open() then
-    M.close()
+  if is_visible() then
+    M.hide()
   else
     M.open()
   end
--- nvim-lazydb/plugin/lazydb.lua
+++ nvim-lazydb/plugin/lazydb.lua
@@ -6,6 +6,10 @@ vim.api.nvim_create_user_command("LazyDBOpen", function()
   require("lazydb").open()
 end, {})
 
-vim.api.nvim_create_user_command("LazyDBClose", function()
-  require("lazydb").close()
+vim.api.nvim_create_user_command("LazyDBHide", function()
+  require("lazydb").hide()
+end, {})
+
+vim.api.nvim_create_user_command("LazyDBKill", function()
+  require("lazydb").kill()
 end, {})
